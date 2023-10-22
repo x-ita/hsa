cab5f48a73972558dcb89a00f4c465e7ab64f2f4
x-ita <145820839+x-ita@users.noreply.github.com>
2023-10-22 16:34:54 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,48 +1,21 @@
-import streamlit as st
+from fastapi import FastAPI
 import torch
 from transformers import MLukeTokenizer, LukeModel
+import pickle
 
-# https://huggingface.co/sonoisa/sentence-luke-japanese-base-lite
-class SentenceLukeJapanese:
-    def __init__(self, model_name_or_path, device=None):
-        self.tokenizer = MLukeTokenizer.from_pretrained(model_name_or_path)
-        self.model = LukeModel.from_pretrained(model_name_or_path)
-        self.model.eval()
+# インスタンス化
+app = FastAPI()
 
-        if device is None:
-            device = "cuda" if torch.cuda.is_available() else "cpu"
-        self.device = torch.device(device)
-        self.model.to(device)
+# 埋め込みモデルの読み込み
+with open('models/sentence-luke-japanese-base-lite', 'rb') as f:
+    model = pickle.load(f)
 
-    def _mean_pooling(self, model_output, attention_mask):
-        token_embeddings = model_output[0] #First element of model_output contains all token embeddings
-        input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
-        return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+# トップページ
+@app.get('/')
+def index():
+    return {"Iris": 'iris_prediction'}
 
-
-    def encode(self, sentences, batch_size=8):
-        all_embeddings = []
-        iterator = range(0, len(sentences), batch_size)
-        for batch_idx in iterator:
-            batch = sentences[batch_idx:batch_idx + batch_size]
-
-            encoded_input = self.tokenizer.batch_encode_plus(batch, padding="longest",
-                                           truncation=True, return_tensors="pt").to(self.device)
-            model_output = self.model(**encoded_input)
-            sentence_embeddings = self._mean_pooling(model_output, encoded_input["attention_mask"]).to('cpu')
-
-            all_embeddings.extend(sentence_embeddings)
-
-        return torch.stack(all_embeddings)
-
-
-MODEL_NAME = "sonoisa/sentence-luke-japanese-base-lite"
-model = SentenceLukeJapanese(MODEL_NAME)
-
-text = 'メロスは激怒した。必ず、かの邪智暴虐の王を除かなければならぬと決意した。メロスには政治がわからぬ。メロスは、村の牧人である。笛を吹き、羊と遊んで暮して来た。けれども邪悪に対しては、人一倍に敏感であった。きょう未明メロスは村を出発し、野を越え山越え、十里はなれた此のシラクスの市にやって来た。メロスには父も、母も無い。女房も無い。十六の、内気な妹と二人暮しだ。この妹は、村の或る律気な一牧人を、近々、花婿として迎える事になっていた。結婚式も間近かなのである。メロスは、それゆえ、花嫁の衣裳やら祝宴の御馳走やらを買いに、はるばる市にやって来たのだ。先ず、その品々を買い集め、それから都の大路をぶらぶら歩いた。メロスには竹馬の友があった。セリヌンティウスである。今は此のシラクスの市で、石工をしている。その友を、これから訪ねてみるつもりなのだ。久しく逢わなかったのだから、訪ねて行くのが楽しみである。歩いているうちにメロスは、まちの様子を怪しく思った。ひっそりしている。もう既に日も落ちて、まちの暗いのは当りまえだが、けれども、なんだか、夜のせいばかりでは無く、市全体が、やけに寂しい。のんきなメロスも、だんだん不安になって来た。路で逢った若い衆をつかまえて、何かあったのか、二年まえに此の市に来たときは、夜でも皆が歌をうたって、まちは賑やかであった筈だが、と質問した。'
-
-tmp = model.encode([text]).detach().clone().numpy()[0, 0]
-
-
-st.title('Iris Classifier')
-st.write(tmp)
+# POST が送信された時（入力）と予測値（出力）の定義
+@app.post('/predict')
+def make_predictions(features: iris):
+    return({'prediction':model.encode(text)})

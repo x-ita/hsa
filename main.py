cb0827a190b840094c2ada7d84df91975b3673f3
x-ita <145820839+x-ita@users.noreply.github.com>
2023-10-27 15:43:42 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 import torch
-from transformers import BertJapaneseTokenizer, BertModel
+from transformers import MLukeTokenizer, LukeModel
 import pandas as pd
 import pickle
 
 # 埋め込みモデルのダウンロード
-# https://huggingface.co/sonoisa/sentence-bert-base-ja-mean-tokens-v2
-class SentenceBertJapanese:
+# https://huggingface.co/sonoisa/sentence-luke-japanese-base-lite
+class SentenceLukeJapanese:
     def __init__(self, model_name_or_path, device=None):
-        self.tokenizer = BertJapaneseTokenizer.from_pretrained(model_name_or_path)
-        self.model = BertModel.from_pretrained(model_name_or_path)
+        self.tokenizer = MLukeTokenizer.from_pretrained(model_name_or_path)
+        self.model = LukeModel.from_pretrained(model_name_or_path)
         self.model.eval()
 
         if device is None:
@@ -23,26 +23,25 @@ class SentenceBertJapanese:
         input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
         return torch.sum(token_embeddings * input_mask_expanded, 1) / torch.clamp(input_mask_expanded.sum(1), min=1e-9)
 
-
     def encode(self, sentences, batch_size=8):
         all_embeddings = []
         iterator = range(0, len(sentences), batch_size)
         for batch_idx in iterator:
             batch = sentences[batch_idx:batch_idx + batch_size]
 
-            encoded_input = self.tokenizer.batch_encode_plus(batch, padding="longest", 
+            encoded_input = self.tokenizer.batch_encode_plus(batch, padding="longest",
                                            truncation=True, return_tensors="pt").to(self.device)
             model_output = self.model(**encoded_input)
             sentence_embeddings = self._mean_pooling(model_output, encoded_input["attention_mask"]).to('cpu')
 
             all_embeddings.extend(sentence_embeddings)
 
-        # return torch.stack(all_embeddings).numpy()
         return torch.stack(all_embeddings)
 
 
-MODEL_NAME = "sonoisa/sentence-bert-base-ja-mean-tokens-v2"  # <- v2です。
-model = SentenceBertJapanese(MODEL_NAME)
+MODEL_NAME = "sonoisa/sentence-luke-japanese-base-lite"
+#model = SentenceLukeJapanese(MODEL_NAME)
+
 
 # チャンクデータの読み込み
 chunk_df = pd.read_pickle('chunk_df.pkl')

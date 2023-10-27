3c6d70851c18a92840a43ec166a995a964ff1abb
x-ita <145820839+x-ita@users.noreply.github.com>
2023-10-27 13:13:57 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+import streamlit as st
+#import pandas as pd
+import requests
+
+st.title('Iris Classifier')
+
+st.text_input('', '')
+st.button('Subnmit')
+
+# 予測の実行
+#response = requests.post("http://localhost:8000/predict", json=iris)
+#prediction = response.json()["prediction"]
+
+# 予測結果の表示
+#st.write('## Prediction')
+#st.write(prediction)
+
+# 予測結果の出力
+#st.write('## Result')
+#st.write('このアイリスはきっと',str(targets[int(prediction)]),'です!')

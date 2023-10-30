3e563e40059d0429e26f84fe649f32482dcd8c47
x-ita <145820839+x-ita@users.noreply.github.com>
2023-10-30 09:49:44 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
+import json
 
 # OpenAI API KEY設定
-OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+#OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
 
 # Vector DB読み込み
 db_dir = '/content/drive/MyDrive/aozorabunko/chroma_db/'
@@ -26,8 +27,11 @@ def index():
 # POST が送信された時（入力）と予測値（出力）の定義
 @app.post('/search_similar')
 def search_similar(query: input_text):
-    search_results = vectordb.similarity_search_with_score(query.text)
-#    similarity = cosine_similarity(model.encode([query.text]).detach().clone().numpy(), vecdb)[0]
-#    sorted_df = chunk_df.assign(similarity=similarity).sort_values('similarity', ascending=False)
-#    return sorted_df.head(3).to_json(orient='records')
-    return pd.DataFrame({'a':['aaa', 'bbb'], 'b':[query.text, query.text]}).to_json(orient='records')
+    search_results = vectordb.similarity_search_with_relevance_scores(query.text, k=3)
+    search_results_json = json.dumps([
+        {
+            'title_author':res[0].metadata['title_author'],
+            'chunk':res[0].page_content,
+            'similarity':round(res[1], 3)
+        } for res in search_results])
+    return search_results_json

a586bf5f0ee0848b85f36e71ce1f2046b969bed1
x-ita <145820839+x-ita@users.noreply.github.com>
2023-10-30 16:48:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,21 @@
 import streamlit as st
-#import pandas as pd
+import pandas as pd
 import requests
 
 st.title('Iris Classifier')
 
-st.text_input('', '')
+text_input = st.text_input('', '')
 st.button('Submit')
 
-# 予測の実行
-#response = requests.post("http://localhost:8000/predict", json=iris)
-#prediction = response.json()["prediction"]
+text_input_json = {
+  'query': text_input
+}
 
-# 予測結果の表示
-#st.write('## Prediction')
-#st.write(prediction)
+# 類似度計算の実行
+response = requests.post("http://localhost:8000/search_similar", json=text_input_json)
+response_json = response.json()
+response_df = pd.read_jsonresponse_jsonorient="records")
 
-# 予測結果の出力
-#st.write('## Result')
-#st.write('このアイリスはきっと',str(targets[int(prediction)]),'です!')
+# 類似度計算結果上位3件の表示
+st.write('## Similar Files')
+st.dataframe(response_df)

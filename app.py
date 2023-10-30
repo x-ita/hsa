c72c1129bff93fb2b67f50017c73afe74bfefad7
x-ita <145820839+x-ita@users.noreply.github.com>
2023-10-30 17:04:33 +0900

    Update app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ text_input_json = {
 }
 
 # 類似度計算の実行
-response = requests.post("http://localhost:8000/search_similar", json=text_input_json)
+response = requests.post("https://homsa.onrender.com/search_similar", json=text_input_json)
 response_json = response.json()
 response_df = pd.read_json(response_json, orient="records")
 

8d2f559bfa7400921e9455e32b2d92e245fc2a16
x-ita <145820839+x-ita@users.noreply.github.com>
2023-11-02 20:48:03 +0900

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 import requests
 
+fastapi_url = st.secrets['FASTAPI_URL']
+
 st.title('文章でファイル検索')
 st.write('青空文庫の昔話・童話の中から，以下の18作品を検索対象ファイルにしました．  \n' + \
          'かちかち山，一寸法師，花咲かじじい，浦島太郎，舌切りすずめ，猿かに合戦，' + \
@@ -17,7 +19,7 @@ text_input_json = {
 
 if st.button('Submit'):
   # 類似度計算を実行し上位3件を取得(FastAPI)
-  response = requests.post("https://hsa.onrender.com/search_qa", json=text_input_json)
+  response = requests.post(fastapi_url, json=text_input_json)
   response_df = pd.read_json(response.json(), orient="records")
   # チャンクに基づく質問応答の表示
   for i in range(3):

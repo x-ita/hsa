6fccc42e6e25d254975ca45c16fea6485d8bd4fd
x-ita <145820839+x-ita@users.noreply.github.com>
2023-11-14 22:12:58 +0900

    Update app.py

--- app.py
+++ app.py
@@ -25,9 +25,9 @@ if st.button('Submit'):
   response = requests.post(fastapi_url, json=input_dict) # 引数jsonでなぜかdict型を渡す
   response_df = pd.read_json(response.json(), orient="records")
   # チャンクに基づく質問応答の表示
-  for i, row in enumerate(response_df.itertuples()):
-    st.write('\n\n回答 ' + str(i+1) + '：\n' + row.answer)
-    st.write('\n\nファイル（作品）：\n' + row.title_author)
-    st.write('\n\n類似度：\n' + str(round(row.similarity, 3)))
-    st.write('\n\nテキスト：\n' + row.text)
+  for i, row in response_df.iterrows():
+    st.write('\n\n回答 ' + str(i+1) + '：  \n' + row['answer'])
+    st.write('\n\nファイル（作品）：\n' + row['title_author'])
+    st.write('\n\n類似度：\n' + str(round(row['similarity'], 3)))
+    st.write('\n\nテキスト：\n' + row['text'])
   
